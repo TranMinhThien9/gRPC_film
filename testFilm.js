a784302dc3747b55e0279324e0ed27c2151a1131
DESKTOP-A4UPJPS\tranm <tranminhthien1199@gmail.com>
2022-04-27 02:58:19 +0700

    gRPC film .

--- testFilm.js
+++ testFilm.js
@@ -1,7 +1,5 @@
 //TESTING
 
-
-
 const client = require('./client')
 
 // client.list({}, (error, actors) => {
@@ -13,41 +11,51 @@ const client = require('./client')
 //     }
 // });
 
-// client.get({ id: 44 }, (error, film) => {
-//     if (!error) {
-//         console.log('Fetch film successfully!')
-//         console.log(film)
-//     } else {
-//         console.error(error)
-//     }
-// });
-
-client.create({
-
-  title: "test",
-  description: "call.request.description",
-  release_year: 2022,
-  language_id: 2,
-  rental_duration: 1,
-  rental_rate: "3.88",
-  length: 128,
-  replacement_cost: "12.99",
-  rating: "",
-  special_features: ""
-}, (error, film) => {
-  if (!error) {
-    console.log('Create film successfully!')
-    console.log('affected rows: ' + film.rows)
-  } else {
-    console.error(error)
-  }
+client.get({ id: 44 }, (error, film) => {
+    if (!error) {
+        console.log('Fetch film successfully!')
+        console.log(film)
+    } else {
+        console.error(error)
+    }
 });
 
-// client.update({ first_name: "Nguyen", last_name: "Huy", actor_id: "22" }, (error, film) => {
-//     if (!error) {
-//         console.log('Update film successfully!')
-//         console.log('affected rows: ' + film.rows)
-//     } else {
-//         console.error(error)
-//     }
+// client.create({
+//   title: "test",
+//   description: "call.request.description",
+//   release_year: 2022,
+//   language_id: 2,
+//   rental_duration: 1,
+//   rental_rate: "3.88",
+//   length: 128,
+//   replacement_cost: "12.99",
+//   rating: "",
+//   special_features: ""
+// }, (error, film) => {
+//   if (!error) {
+//     console.log('Create film successfully!')
+//     console.log('affected rows: ' + film.rows)
+//   } else {
+//     console.error(error)
+//   }
+// });
+
+// client.update({
+//   title: "test",
+//   description: "call.request.description",
+//   release_year: 2022,
+//   language_id: 2,
+//   rental_duration: 1,
+//   rental_rate: "3.88",
+//   length: 128,
+//   replacement_cost: "12.99",
+//   rating: "",
+//   special_features: ""
+// }, (error, film) => {
+//   if (!error) {
+//     console.log('Update film successfully!')
+//     console.log('affected rows: ' + film.rows)
+//   } else {
+//     console.error(error)
+//   }
 // });
